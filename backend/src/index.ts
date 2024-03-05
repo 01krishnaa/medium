13836f64b5b96789216308237995049839f1d3cd
Krishna Singh <krishnasingh781630@gmail.com>
2024-03-06 05:14:30 +0530

    database init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,40 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono().basePath("/api/v1");
 
-export default app
+
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/user/signup", (c) => {
+  return c.text("signup");
+});
+
+app.post("/user/signin", (c) => {
+  return c.text("signin");
+});
+
+app.post("/blog", (c) => {
+  return c.text("blog");
+});
+
+app.put("/blog", (c) => {
+  return c.text("blog");
+});
+
+app.get("/blog/:id", (c) => {
+  const { id } = c.req.param();
+  console.log(id);
+  return c.text("get blog");
+});
+
+app.get("/blog/bulk", (c) => {
+  return c.text("blog");
+});
+
+export default app;
