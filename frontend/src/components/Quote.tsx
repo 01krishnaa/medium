b379a4276e556b09b6f018f776f7dce7fba8370a
Krishna Singh <krishnasingh781630@gmail.com>
2024-03-08 01:46:16 +0530

    frontend init

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,9 @@
+export const Quote = () => {
+  return (
+    <>
+      <div className="h-screen bg-slate-200 flex items-center justify-center">
+        <div>quiorwj</div>
+      </div>
+    </>
+  );
+};
