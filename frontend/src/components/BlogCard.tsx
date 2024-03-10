a382bea67ae3f484e0211ea25d7839c6a694171b
Krishna Singh <krishnasingh781630@gmail.com>
2024-03-11 01:41:01 +0530

    blogcard component added

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,7 +1,36 @@
-const BlogCard = () => {
-  return (
-    <div>BlogCard</div>
-  )
+interface BlogCardProps {
+  authorName: string;
+  title: string;
+  content: string;
+  publishedDate: string;
 }
 
-export default BlogCard
+const BlogCard = ({
+  authorName,
+  title,
+  content,
+  publishedDate,
+}: BlogCardProps) => {
+  return (
+    <div className="w-1/2 mx-auto p-5">
+      <div className="flex">
+        <Avatar author={authorName} />
+        <div className="mx-4"> {authorName} </div>
+        <div className="text-gray-500">{publishedDate}</div>
+      </div>
+      <div className="font-bold text-3xl">{title}</div>
+      <div>{content.slice(0, 180) + "..."}</div>
+      <div className="border-b border-slate-300 py-3">
+        {Math.ceil(content.length / 100) + " minute(s) read"}
+      </div>
+    </div>
+  );
+};
+
+function Avatar({ author }: { author: string }) {
+  return (
+    <div className="bg-slate-400 w-6 text-center rounded-full">{author[0]}</div>
+  );
+}
+
+export default BlogCard;
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,8 +1,58 @@
+import BlogCard from "../components/BlogCard";
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
-  )
-}
+    <div>
+      <BlogCard
+        authorName="Krishna Singh"
+        title="How ugly websites make 5000 dollars a month ?"
+        content={
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Error blanditiis quasi nam magni doloremque similique assumenda. Ducimus quaerat explicabo quasi ratione quis. Quaerat obcaecati voluptas sapiente distinctio, numquam reiciendis odio?"
+        }
+        publishedDate="2nd June 2043"
+      />
+      <BlogCard
+        authorName="Krishna Singh"
+        title="How ugly websites make 5000 dollars a month ?"
+        content={
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Error blanditiis quasi nam magni doloremque similique assumenda. Ducimus quaerat explicabo quasi ratione quis. Quaerat obcaecati voluptas sapiente distinctio, numquam reiciendis odio?"
+        }
+        publishedDate="2nd June 2043"
+      />
+      <BlogCard
+        authorName="Krishna Singh"
+        title="How ugly websites make 5000 dollars a month ?"
+        content={
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Error blanditiis quasi nam magni doloremque similique assumenda. Ducimus quaerat explicabo quasi ratione quis. Quaerat obcaecati voluptas sapiente distinctio, numquam reiciendis odio?"
+        }
+        publishedDate="2nd June 2043"
+      />
+      <BlogCard
+        authorName="Krishna Singh"
+        title="How ugly websites make 5000 dollars a month ?"
+        content={
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Error blanditiis quasi nam magni doloremque similique assumenda. Ducimus quaerat explicabo quasi ratione quis. Quaerat obcaecati voluptas sapiente distinctio, numquam reiciendis odio?"
+        }
+        publishedDate="2nd June 2043"
+      />
+      <BlogCard
+        authorName="Krishna Singh"
+        title="How ugly websites make 5000 dollars a month ?"
+        content={
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Error blanditiis quasi nam magni doloremque similique assumenda. Ducimus quaerat explicabo quasi ratione quis. Quaerat obcaecati voluptas sapiente distinctio, numquam reiciendis odio?"
+        }
+        publishedDate="2nd June 2043"
+      />
+      <BlogCard
+        authorName="Krishna Singh"
+        title="How ugly websites make 5000 dollars a month ?"
+        content={
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Error blanditiis quasi nam magni doloremque similique assumenda. Ducimus quaerat explicabo quasi ratione quis. Quaerat obcaecati voluptas sapiente distinctio, numquam reiciendis odio?"
+        }
+        publishedDate="2nd June 2043"
+      />
+    </div>
+  );
+};
 
-export default Blogs
+export default Blogs;
